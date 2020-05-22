c4a64f0e65f3491f6a38a5654f4a0c620b975712
osagie obaretin <osagie.codes@gmail.com>
2020-05-22 05:34:58 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ var height = "74"
 if (index.height === 74) {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+    console.log("Expected height to be correct")
 }
 
 
