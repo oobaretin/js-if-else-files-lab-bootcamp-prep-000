d7c4785054a9bb65ee716e42e03780a0b5d3963a
osagie obaretin <osagie.codes@gmail.com>
2020-05-22 05:25:23 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 var name = "Susan"
 var height = "74"
 
-if (height==="74") {
-  console.log("The height is correct")
+
+if (index.height === 70) {
+    console.log("The height is correct")
+} else {
+    console.log("Expected: 70, Received: "+index.height)
 }
+
 
 
 // Don't worry about this
