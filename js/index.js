ff28b0c80aea534a4eb57891d56d2556a1cb0e06
osagie obaretin <osagie.codes@gmail.com>
2020-05-22 05:55:26 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,17 +1,6 @@
 var name = "Joe"
 var height = "74"
 
-if (index.name === "Joe") {
-    console.log("The name is correct")
-} 
-
-
-
-if (index.height === 74) {
-    console.log("The height is correct")
-}
-
-
 
 
 
