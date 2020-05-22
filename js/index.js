1412549a3caa345bade178edf8bf9b81978b08b9
osagie obaretin <osagie.codes@gmail.com>
2020-05-22 05:54:19 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,16 +3,12 @@ var height = "74"
 
 if (index.name === "Joe") {
     console.log("The name is correct")
-} else {
-    console.log("Expected: Joe, Received: "+index.name)
-}
+} 
 
 
 
 if (index.height === 74) {
     console.log("The height is correct")
-} else {
-    console.log("Expected: 74, Received: "+index.height)
 }
 
 
